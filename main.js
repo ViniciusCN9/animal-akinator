094e9174f8262f82b9ce052b211f4c39d3385eb6
Guilherme-Zamboni <guilherme1zamb@hotmail.com>
2023-08-31 22:20:37 -0300

    backend

---
+++ main.js
@@ -0,0 +1,85 @@
+let input = "input";
+
+print("Esse animal é um mamífero? ");
+input = "get input";
+
+if (input == sim) {
+    print("Esse animal é um quadrúpede? ");
+    input = "get input";
+
+    if (input == sim) {
+        print("Esse animal é carnívoro? ");
+        input = "get input";
+
+        if (input == sim) {
+            print("Esse animal é um leão");
+            break;
+        }
+
+        else {
+            print("Esse animal é um cavalo");
+            break;
+        }
+    }
+
+    else {
+        print("Esse animal é um bípede? ");
+        input = "get input";
+        
+        if (input == sim) {
+            print("Esse animal é onívoro? ");
+            input = "get input";
+    
+            if (input == sim) {
+                print("Esse animal é um homem");
+                break;
+            }
+    
+            else {
+                print("Esse animal é um macaco");
+                break;
+            }
+        }
+    
+        else {
+            print("Esse animal é voador? ");
+            input = "get input";
+    
+            if (input == sim) {
+                print("Esse animal é um morcego");
+                break;
+            }
+    
+            else {
+                print("Esse animal é uma baleia");
+                break;
+            }
+        }
+    }
+}
+
+else {
+print("Esse animal é uma ave?");
+input = "get input";
+
+
+
+
+}
+if (input = sim) {
+    print("Esse animal é do tipo Não-voador?")
+    input = "get input";
+
+    if (input = sim) {
+        print("Esse animal é do tipo Tropical?")
+        input = "get input";
+
+        if (input = sim) {
+            print("Esse animal é um Avestruz?")
+            input = "get input";
+
+            else
+            print("Esse animal é do tipo polar, e é um Pinguim");
+        }
+
+    
